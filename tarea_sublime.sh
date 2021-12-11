9e1ee119063c67f5e0b5991fc1890b7e26d87fbe
Alejo505 <94197353+Alejo505@users.noreply.github.com>
2021-12-10 19:36:02 -0500

    Update tarea_sublime.sh
    tarea, primera parte enviada desde el sublime

---
+++ tarea_sublime.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# la dirección del directorio donde se trabajó es Desktop/CSB/CSB/unix/data
+#parte 1 del deber. usar batch netsize.sh n1.txt
+
+echo "Filename: ../data/Saavedra2013/n1.txt"
+
+echo "Number of rows:"  $( cat $1 | wc -l)
+
+echo "Number of columns:" $( head -n 1 $1 | tr -d " " | tr -d "\n" | wc -c )
+
+# usé echo para imprimir como texto el nombre del archivo y el lugar donde se
+# encuentra. Para el número de filas us echo para imprimir "number of rows" y
+# a su vez la cantidad, para eso uso el $() que lo toma como una variable que me
+# permite imprimir. Con $1 digo que tengo una variable la cual es n1.txt.
+# El con el cat le pido que imprima el arhivo y a su vez con
+# wc -l cuenta el número de filas del archivo seleccionado.
+# para las columnas también uso echo, head -n 1 $1 me muestra la primera fila del
+#archivo seleccionado y añado más ordenes usando el "|" , tr - "" borra los espacios
+# del archivo para q no los tome en cuenta y con \n quito un espacio extra
+# finalmente wc-c muestra la cantidad de carácteres que son los números en este caso.
